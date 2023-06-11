3f4aad3cba078ea028b73557105f64cd7898f9cf
karpych1717 <admkarpych1717@gmail.com>
2023-06-12 00:08:25 +0300

    add remove

--- homework_35/src/SWAPI/SWAPIUI/Card/index.jsx
+++ homework_35/src/SWAPI/SWAPIUI/Card/index.jsx
@@ -1,15 +1,21 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import styles from './styles.module.css'
+
+import { swapiSlice } from '../../store'
 
 function Card (props) {
   const responce = useSelector(store => store.body)
-  console.log(responce)
-  console.log(typeof responce.data)
+  const dispatch = useDispatch()
+
+  function handleClear () {
+    dispatch(swapiSlice.actions.clear())
+  }
 
   return (
     <div className={styles.responceContainer}>
       <span className={styles.meta}>{responce.request}</span>
       <span className={styles.code}>{JSON.stringify(responce.data, null, 2)}</span>
+      <button type='button' className={styles.clearButton} onClick={handleClear}>Clear</button>
     </div>
   )
 }
